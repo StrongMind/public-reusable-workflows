c1d9e1b7fd1a74dd57249ddb19dc381a22adcec0
Paul Shippy <paul.shippy@strongmind.com>
2023-07-13 15:18:14 -0700

    BEL-1301 make redis configurable
    - https://strongmind.atlassian.net/browse/BEL-1301
    
    Co-authored-by: Mark Ng <mark@markng.co.uk>

--- deployment/src/strongmind_deployment/redis.py
+++ deployment/src/strongmind_deployment/redis.py
@@ -9,6 +9,8 @@ class RedisComponent(pulumi.ComponentResource):
         super().__init__('strongmind:global_build:commons:redis', name, None, opts)
         self.kwargs = kwargs
         self.env_vars = self.kwargs.get('env_vars', {})
+        self.node_type = self.kwargs.get('node_type', 'cache.t4g.small')
+        self.num_cache_nodes = self.kwargs.get('num_cache_nodes', 1)
 
         self.env_name = os.environ.get('ENVIRONMENT_NAME', 'stage')
 
@@ -27,9 +29,9 @@ class RedisComponent(pulumi.ComponentResource):
             "redis",
             cluster_id=project_stack,
             engine="redis",
-            node_type="cache.t4g.small",
+            node_type=self.node_type,
             engine_version="7.0",
-            num_cache_nodes=1,
+            num_cache_nodes=self.num_cache_nodes,
             parameter_group_name="default.redis7",
             port=6379,
             tags=self.tags,
--- deployment/src/tests/test_redis.py
+++ deployment/src/tests/test_redis.py
@@ -25,11 +25,17 @@ def describe_a_pulumi_redis_component():
         return get_pulumi_mocks(faker)
 
     @pytest.fixture
+    def component_arguments():
+        return {}
+
+    @pytest.fixture
     def sut(pulumi_set_mocks,
+            component_arguments
             ):
         import strongmind_deployment.redis
 
         sut = strongmind_deployment.redis.RedisComponent("redis",
+                                                         **component_arguments
                                                          )
         return sut
 
@@ -49,17 +55,35 @@ def describe_a_pulumi_redis_component():
             return assert_output_equals(sut.cluster.engine_version, "7.0")
 
         @pulumi.runtime.test
-        def test_it_has_node_type(sut):
-            return assert_output_equals(sut.cluster.node_type, "cache.t4g.small")
-
-        @pulumi.runtime.test
-        def it_has_num_cache_nodes(sut):
-            return assert_output_equals(sut.cluster.num_cache_nodes, 1)
-
-        @pulumi.runtime.test
         def it_has_parameter_group_name(sut):
             return assert_output_equals(sut.cluster.parameter_group_name, "default.redis7")
 
         @pulumi.runtime.test
         def it_has_port(sut):
             return assert_output_equals(sut.cluster.port, 6379)
+
+        def describe_with_defaults():
+            @pulumi.runtime.test
+            def test_it_has_node_type(sut):
+                return assert_output_equals(sut.cluster.node_type, "cache.t4g.small")
+
+            @pulumi.runtime.test
+            def it_has_num_cache_nodes(sut):
+                return assert_output_equals(sut.cluster.num_cache_nodes, 1)
+
+
+        def describe_with_scaling_overrides():
+            @pytest.fixture
+            def component_arguments():
+                return {
+                    "node_type": "cache.t3.small",
+                    "num_cache_nodes": 2,
+                }
+
+            @pulumi.runtime.test
+            def it_has_node_type(sut, component_arguments):
+                return assert_output_equals(sut.cluster.node_type, "cache.t3.small")
+
+            @pulumi.runtime.test
+            def it_has_num_cache_nodes(sut, component_arguments):
+                return assert_output_equals(sut.cluster.num_cache_nodes, 2)
