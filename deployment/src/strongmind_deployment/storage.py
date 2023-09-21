518adfd264fce643b5f2d871e9463a323db97e6e
Joshua Gleason <joshua.gleason@strongmind.com>
2023-09-21 11:08:01 -0700

    Adding option to add a public or private S3 bucket; updated tests

--- deployment/src/strongmind_deployment/storage.py
+++ deployment/src/strongmind_deployment/storage.py
@@ -29,7 +29,7 @@ class StorageComponent(pulumi.ComponentResource):
                                                                         ))
         self.bucket_public_access_block = aws.s3.BucketPublicAccessBlock("bucket_public_access_block",
                                                                          bucket=self.bucket.id,
-                                                                         block_public_acls=False)
+                                                                         block_public_acls= kwargs.get('storage_private', True))
 
         acl_opts = pulumi.ResourceOptions(
             depends_on=[self.bucket_ownership_controls, self.bucket_public_access_block])  # pragma: no cover
