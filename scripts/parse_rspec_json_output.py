e51be0ad78c91aad76b90642233cb111b53aac4c
Joshua Gleason <joshua.gleason@strongmind.com>
2024-02-16 11:39:47 -0700

    Add actions and script to parse rspec output

---
+++ scripts/parse_rspec_json_output.py
@@ -0,0 +1,30 @@
+import json
+import sys
+
+def main():
+    with open('rspec-output.json', 'r') as file:
+        data = json.load(file)
+
+    specific_pending_messages = [
+        "Temporarily skipped with xit",
+        "Not yet implemented",
+        "Add a hash of attributes"
+    ]
+
+    skipped_tests = [
+        example for example in data['examples']
+        if example['status'] == 'pending' and
+           any(msg in example.get('pending_message', '') for msg in specific_pending_messages)
+    ]
+
+    if skipped_tests:
+        print("Skipped tests found with specific pending messages:")
+        for test in skipped_tests:
+            print(f"{test['full_description']} - Pending Reason: {test['pending_message']}")
+        sys.exit(1) 
+    else:
+        print("No skipped tests found with specific pending messages.")
+        sys.exit(0) 
+
+if __name__ == "__main__":
+    main()
