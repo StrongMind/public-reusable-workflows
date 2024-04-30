f9e425e9b8ecaedd85ffda37bc72f003766f87a9
Trey Gonzalez <trey.gonzalez@strongmind.com>
2024-04-30 11:44:13 -0700

    too many changes.. formatting and re organizing resources

--- deployment/src/strongmind_deployment/batch.py
+++ deployment/src/strongmind_deployment/batch.py
@@ -8,14 +8,14 @@ from pulumi_aws import cloudwatch
 
 class BatchComponent(pulumi.ComponentResource):
     def __init__(self, name, **kwargs):
-        #super().__init__("custom:module:BatchComponent", name, {})
+        super().__init__("custom:module:BatchComponent", name, {})
         self.env_name = os.environ.get('ENVIRONMENT_NAME', 'stage')
         self.kwargs = kwargs
-        self.name = name
-        self.max_vcpus = kwargs.get('max_vcpus', None)
+        self.max_vcpus = self.kwargs.get('max_vcpus', 0.25)
+        self.max_memory = self.kwargs.get('max_memory', 2048)
+        self.command = self.kwargs.get('command', '["echo", "hello world"]')
+        self.cron = self.kwargs.get('cron', 'cron(0 0 * * ? *)')
         #self.image_id = image_id
-        self.service_role = kwargs.get('service_role', None)
-        priority = 1
         #self.json = json
 
 
@@ -23,6 +23,7 @@ class BatchComponent(pulumi.ComponentResource):
         region = "us-west-2"
         config = pulumi.Config()
         project = pulumi.get_project()
+        self.project_stack = f"{project}-{stack}"
 
         git_root = subprocess.check_output(['git', 'rev-parse', '--show-toplevel'], cwd=".").decode('utf-8').strip()
         with open(f'{git_root}/CODEOWNERS', 'r') as file:
@@ -55,63 +56,123 @@ class BatchComponent(pulumi.ComponentResource):
         default_subnet = []
         #default_subnet.append(default_vpc.public_subnet_ids) 
         print(f"subnet is: {default_subnets.ids}")
-        create_env = aws.batch.ComputeEnvironment(f"{name}-batch",
-        compute_environment_name=f"{name}-batch",
-        compute_resources=aws.batch.ComputeEnvironmentComputeResourcesArgs( max_vcpus=self.max_vcpus,
-            security_group_ids=default_sec_group,
-            subnets=default_subnets.ids,
-            type="FARGATE",
+        execution_role = aws.iam.Role(
+            f"{self.project_stack}-execution-role",
+            name=f"{self.project_stack}-execution-role",
+            assume_role_policy=json.dumps(
+                {
+                    "Version": "2008-10-17",
+                    "Statement": [
+                        {
+                            "Sid": "",
+                            "Effect": "Allow",
+                            "Principal": {"Service": "ecs-tasks.amazonaws.com"},
+                            "Action": "sts:AssumeRole",
+                        }
+                    ],
+                }
             ),
-        type="MANAGED",
-        tags=tags,
-        service_role=self.service_role
-        ) 
-
-
-        sch_policy = aws.batch.SchedulingPolicy(f"{name}-sch_policy",
-         fair_share_policy=aws.batch.SchedulingPolicyFairSharePolicyArgs(
-             share_distributions=[
-                 aws.batch.SchedulingPolicyFairSharePolicyShareDistributionArgs(
-                     share_identifier="A1*",
-                     weight_factor=1
-                    )
-                ]
+            tags=tags,
+            opts=pulumi.ResourceOptions(parent=self),
+        )
+        execution_policy = aws.iam.RolePolicy(
+            f"{self.project_stack}-execution-policy",
+            name=f"{self.project_stack}-execution-policy",
+            role=execution_role.id,
+            policy=json.dumps(
+                {
+                    "Version": "2012-10-17",
+                    "Statement": [
+                        {
+                            "Action": [
+                                "ecs:*",
+                                "ecr:GetAuthorizationToken",
+                                "ecr:BatchCheckLayerAvailability",
+                                "ecr:GetDownloadUrlForLayer",
+                                "ecr:BatchGetImage",
+                                "ecr:GetRepositoryPolicy",
+                                "ecr:DescribeRepositories",
+                                "ecr:ListImages",
+                                "ecr:DescribeImages",
+                                "ecr:InitiateLayerUpload",
+                                "ecr:UploadLayerPart",
+                                "ecr:CompleteLayerUpload",
+                                "ecr:PutImage",
+                                "logs:CreateLogStream",
+                                "logs:PutLogEvents",
+                                "secretsmanager:GetSecretValue",
+                            ],
+                            "Effect": "Allow",
+                            "Resource": "*",
+                        }
+                    ],
+                }
             ),
-            tags=tags
+            opts=pulumi.ResourceOptions(parent=self),
         )
 
-        queue = aws.batch.JobQueue(f"{name}-queue",
+        create_env = aws.batch.ComputeEnvironment(f"{self.project_stack}-batch",
+            compute_environment_name=f"{self.project_stack}-batch",
+            compute_resources=aws.batch.ComputeEnvironmentComputeResourcesArgs( max_vcpus=self.max_vcpus,
+                security_group_ids=default_sec_group,
+                subnets=default_subnets.ids,
+                type="FARGATE",
+                ),
+            type="MANAGED",
+            tags=tags,
+            service_role="arn:aws:iam::221871915463:role/aws-service-role/batch.amazonaws.com/AWSServiceRoleForBatch",
+            ) 
+
+
+        sch_policy = aws.batch.SchedulingPolicy(f"{self.project_stack}-sch_policy",
+            name=f"{self.project_stack}-sch_policy",
+            fair_share_policy=aws.batch.SchedulingPolicyFairSharePolicyArgs(
+                share_distributions=[
+                    aws.batch.SchedulingPolicyFairSharePolicyShareDistributionArgs(
+                        share_identifier="A1*",
+                        weight_factor=1
+                        )
+                    ]
+                ),
+                tags=tags
+            )
+
+        queue = aws.batch.JobQueue(f"{self.project_stack}-queue",
+            name=f"{self.project_stack}-queue",
             compute_environments=[create_env.arn],
-            priority=priority,
+            priority=1,
             state="ENABLED",
             tags=tags
         )
 
         CONTAINER_IMAGE = os.environ['CONTAINER_IMAGE']
-        vcpu = 0.25
-        memory = 2048
+        vcpu = self.max_vcpus
+        memory = self.max_memory
+        command = self.command
 
+        lambda_execution_role = execution_role.arn.apply(lambda arn: arn)
         containerProperties = {
-            "command": ["echo", "hello world"],
+            "command": command,
             "image": CONTAINER_IMAGE,
             "resourceRequirements": [
                 {"type": "MEMORY", "value": str(memory)},
                 {"type": "VCPU", "value": str(vcpu)}
             ],
-            "executionRoleArn": "arn:aws:iam::221871915463:role/ecsTaskExecutionRole",
-            "jobRoleArn": "arn:aws:iam::221871915463:role/ecsTaskExecutionRole",
+            "executionRoleArn": f"{lambda_execution_role}",
+            "jobRoleArn": f"{lambda_execution_role}",
         }
 
         jsonDef = json.dumps(containerProperties)
 
         definition = aws.batch.JobDefinition(
-            f"{name}-definition",
+            f"{self.project_stack}-definition",
+            name = f"{self.project_stack}-definition",
             type="container",
             platform_capabilities=["FARGATE"],
-            container_properties=jsonDef
+            container_properties=jsonDef,
+            tags=tags
         )
 
-        schedule_expression = "cron(0 0 * * ? *)"
         event_pattern = {
             "source": ["aws.batch"],
             "detail-type": ["Batch Job State Change"],
@@ -121,29 +182,22 @@ class BatchComponent(pulumi.ComponentResource):
         }
 
         rule = aws.cloudwatch.EventRule(
-            f"{name}-eventbridge-rule",
-            schedule_expression=schedule_expression,
+            f"{self.project_stack}-eventbridge-rule",
+            name=f"{self.project_stack}-eventbridge-rule",
+            schedule_expression=self.cron,
             state="ENABLED",
             #event_pattern=event_pattern,
             tags=tags
         )
 
         event_target = aws.cloudwatch.EventTarget(
-            f"{name}-event-target",
+            f"{self.project_stack}-event-target",
             rule=rule.name,
             arn=queue.arn,
             role_arn="arn:aws:iam::221871915463:role/ecsEventsRole",
             batch_target=cloudwatch.EventTargetBatchTargetArgs(
-                job_definition=definition,
+                job_definition=definition.arn,
                 job_name=rule.name,
                 job_attempts=1
                 ),
-        )
-        
-        ecr_repo = aws.ecr.Repository(
-            f"{name}-ecr-repo",
-            image_scanning_configuration=aws.ecr.RepositoryImageScanningConfigurationArgs(
-                scan_on_push=True
-            ),
-            tags=tags
         )
